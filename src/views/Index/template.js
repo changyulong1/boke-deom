cd4c8e7cabbcc108505db0bbd330c7c277e170a8
xiaocao <853044263@qq.com>
2022-03-21 14:17:54 +0800

    安装element-ui 和简单的使用

--- src/views/Index/template.js
+++ src/views/Index/template.js
@@ -3,5 +3,20 @@ export default {
         return{
             name:"首页"
         }
+    },
+    methods:{
+        open() {
+            this.$message('可以维嘉吃饭了');
+        },
+
+        openVn() {
+            const h = this.$createElement;
+            this.$message({
+                message: h('p', null, [
+                    h('span', null, '内容可以是 '),
+                    h('i', { style: 'color: teal' }, '我是小明')
+                ])
+            });
+        }
     }
 }
--- src/views/Index/template.vue
+++ src/views/Index/template.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     {{ name }}
+    <el-button  @click="open">默认按钮</el-button>
+    <el-button type="danger"  @click="openVn">危险按钮</el-button>
   </div>
 </template>
 
